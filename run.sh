52d755396295b0788bc8667c8af9af82865b714d
ENDOH takanao <djmchl@gmail.com>
2014-10-19 07:47:13 +0700

    fix GOPATH

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 echo "$WERCKER_GO_APPENGINE_DEPLOY_PASSWORD" > "$WERCKER_STEP_TEMP/password"
 
-cd $WERCKER_SOURCE_DIR
+debug 'Set $GOPATH'
+export GOPATH="$WERCKER_SOURCE_DIR"
+
+debug 'Display $GOPATH'
+goapp env GOPATH
 
 debug 'Starting deployment'
-appcfg.py update `find . -maxdepth 4 -name app.yaml | head -n1` --email="$WERCKER_GO_APPENGINE_DEPLOY_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
+appcfg.py update `find src/$WERCKER_GIT_DOMAIN/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY -maxdepth 4 -name app.yaml | head -n1` --email="$WERCKER_GO_APPENGINE_DEPLOY_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
 
 success 'Finished'
