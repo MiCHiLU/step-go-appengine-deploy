858ab666981663413d6774f1bc86038a78042675
ENDOH takanao <djmchl@gmail.com>
2015-01-29 12:06:28 +0700

    fix for modules

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ debug 'Display $GOPATH'
 goapp env GOPATH
 
 debug 'Starting deployment'
-appcfg.py update `find src/$WERCKER_GIT_DOMAIN/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY -maxdepth 4 -name app.yaml | head -n1` --email="$WERCKER_GO_APPENGINE_DEPLOY_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
+(cd `find src/$WERCKER_GIT_DOMAIN/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY -maxdepth 4 -name app.yaml | head -n1` && appcfg.py update `grep "^application:" -l -null *.yaml` --email="$WERCKER_GO_APPENGINE_DEPLOY_EMAIL" --passin < "$WERCKER_STEP_TEMP/password")
 
 success 'Finished'
